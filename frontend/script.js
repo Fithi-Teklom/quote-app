df7de20adc128aff9b68d1e6cffda9ff8f205e06
Fithi Teklom <fthiteklom@gmail.com>
2026-09-19 02:26:20 +0100

    Add quote form to frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,36 @@
 const quoteElement = document.querySelector("#quote");
 const button = document.querySelector("#quote-button");
+const form = document.querySelector("#quote-form");
 
-button.addEventListener("click", async () => {
-  const response = await fetch("https://hx5vmh3uvnltexmaepu8xr17.trainees.hosting.cyf.academy");
+const BACKEND_URL = "https://hx5vmh3uvnltexmaepu8xr17.trainees.hosting.cyf.academy";
+
+async function getQuote() {
+  const response = await fetch(BACKEND_URL);
   const quote = await response.json();
 
   quoteElement.textContent = `"${quote.quote}" - ${quote.author}`;
+}
+
+button.addEventListener("click", getQuote);
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const quote = document.querySelector("#new-quote").value;
+  const author = document.querySelector("#new-author").value;
+
+  await fetch(BACKEND_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      quote: quote,
+      author: author,
+    }),
+  });
+
+  form.reset();
+
+  await getQuote();
 });
