46ac3d6a5de49afdfe94adefd6bfafa3da2921e4
Fithi Teklom <fthiteklom@gmail.com>
2026-09-19 01:59:04 +0100

    Fix frontend backend URL

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,9 @@
-"https://hx5vmh3uvnltexmaepu8xr17.trainees.hosting.cyf.academy"const quoteElement = document.querySelector("#quote");
+const quoteElement = document.querySelector("#quote");
 const button = document.querySelector("#quote-button");
 
 button.addEventListener("click", async () => {
-  const response = await fetch("http://172.17.64.211:3000");^X
-
+  const response = await fetch("https://hx5vmh3uvnltexmaepu8xr17.trainees.hosting.cyf.academy");
   const quote = await response.json();
 
   quoteElement.textContent = `"${quote.quote}" - ${quote.author}`;
 });
-
