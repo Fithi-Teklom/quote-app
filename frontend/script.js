f0c656b827b162ee5107d1fb67c428f189d813a2
Fithi Teklom <fthiteklom@gmail.com>
2026-09-19 01:25:28 +0100

    Connect frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,11 @@
-const quoteElement = document.querySelector("#quote");
+"https://hx5vmh3uvnltexmaepu8xr17.trainees.hosting.cyf.academy"const quoteElement = document.querySelector("#quote");
 const button = document.querySelector("#quote-button");
 
 button.addEventListener("click", async () => {
-  const response = await fetch("http://172.17.64.211:3000");
+  const response = await fetch("http://172.17.64.211:3000");^X
+
   const quote = await response.json();
 
   quoteElement.textContent = `"${quote.quote}" - ${quote.author}`;
 });
+
